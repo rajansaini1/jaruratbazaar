231c184f834301671c5f55eb7baa18871ce7e587
rajan saini <rajanynrsaini@gmail.com>
2019-11-21 17:55:01 -0800

    @rajan-modify

--- database/tbtempdata.sql
+++ database/tbtempdata.sql
@@ -46,3 +46,4 @@ INSERT INTO `bazaarapp_tempdatatable` (`table_id`, `email`, `first_name`, `last_
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+bazaarapp_tempdatatable
